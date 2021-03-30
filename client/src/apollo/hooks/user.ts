7fd6ae668def77f79159903f7b445838df161e17
lu4ezar <misha-shisha@mail.ru>
2021-03-31 00:58:48 +0700

    update user hooks

--- client/src/apollo/hooks/user.ts
+++ client/src/apollo/hooks/user.ts
@@ -1,80 +1,34 @@
-import jwtDecode from 'jwt-decode';
-import { ApolloError, gql, useMutation } from '@apollo/client';
-// import { CREATE_USER, AUTH_USER } from '../mutations/user';
-import React from 'react';
-// import { isLoggedInVar } from "../cache";
+import { gql, useMutation } from '@apollo/client';
+import { errorVar, isLoggedInVar } from '..';
 
-const CREATE_USER = gql`
-  mutation createUser($input: CreateUserInput!) {
-    createUser(input: $user) {
+const LOGIN_USER = gql`
+  mutation loginUser($input: UserInput!) {
+    loginUser(input: $input) {
       token
     }
   }
 `;
-
-const AUTH_USER = gql`
-  mutation authUser($input: AuthUserInput!) {
-    createUser(input: $user) {
-      token
-    }
-  }
-`;
-
-export const useGetUserFromToken = () => {
-  let [user, setUser] = React.useState(null);
-  const getUser = () => {
-    const token = localStorage.getItem('token');
-    user = jwtDecode(token as string);
-    setUser(user);
-    console.log(user);
-    return user;
-  };
-  React.useEffect(() => {
-    window.addEventListener('storage', getUser);
-    return () => window.removeEventListener('storage', getUser);
-  });
-  return [user];
-};
 
 export const useLoginMutation = () => {
-  const [loginUser, { loading, error }] = useMutation(AUTH_USER, {
+  const [loginUser, { loading, error }] = useMutation(LOGIN_USER, {
     onCompleted: (data) => {
       if (data) {
         const {
           loginUser: { token },
         } = data;
-        const user = jwtDecode(token);
-        localStorage.setItem('user', JSON.stringify(user));
-        // isLoggedInVar(true);
+        localStorage.setItem('token', JSON.stringify(token));
+        isLoggedInVar(true);
       }
     },
     errorPolicy: 'all',
     onError: (err) => {
-      localStorage.removeItem('user');
-      throw new ApolloError(err);
+      localStorage.removeItem('token');
+      isLoggedInVar(true);
+      errorVar(err.message);
     },
   });
   return {
     loginUser,
-    loading,
-    error,
-  };
-};
-
-export const useCreateUserMutation = () => {
-  const [createUser, { loading, error }] = useMutation(CREATE_USER, {
-    onCompleted: (data) => {
-      if (data) {
-        const {
-          createUser: { token },
-        } = data;
-        const user = jwtDecode(token);
-        localStorage.setItem('user', JSON.stringify(user));
-      }
-    },
-  });
-  return {
-    createUser,
     loading,
     error,
   };
