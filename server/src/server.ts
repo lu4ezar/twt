f529b681a6799a21eb0563d55b97bccb2f50ffef
lu4ezar <misha-shisha@mail.ru>
2021-03-31 20:10:19 +0700

    server: add permissions, put user on context

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,35 @@
 import { ApolloServer } from 'apollo-server-express';
+import { Request, Response } from 'express';
+import { applyMiddleware } from 'graphql-middleware';
+import jwt from 'jsonwebtoken';
 import schema from './graphql/schema';
 import dataSources from './graphql/datasources';
 import db from './db';
+import permissions from './graphql/permissions';
 
 const server = new ApolloServer({
-  schema,
   dataSources,
-  context: () => ({
-    db,
-  }),
+  schema: applyMiddleware(schema, permissions),
+  context: async function createContext({
+    req,
+    res,
+  }: {
+    req: Request;
+    res: Response;
+  }) {
+    let user;
+
+    try {
+      if (req && req.headers.authorization) {
+        const token = (req.headers.token as string).split(' ')[1] || '';
+        user = jwt.verify(token, process.env.SECRET as string);
+        return { db, res, user };
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+    return { db, res, user: null };
+  },
 });
 
 export default server;
