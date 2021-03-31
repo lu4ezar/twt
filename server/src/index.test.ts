2aa914d6b7c04c04fa9ccc7bd967bf0e28bc1a0e
lu4ezar <misha-shisha@mail.ru>
2021-03-31 11:34:14 +0700

    remove some noise

--- server/src/index.test.ts
+++ server/src/index.test.ts
@@ -1,6 +1,6 @@
-import { setupWorker, graphql } from 'msw';
-import { server } from './index';
-import { gql } from 'apollo-server-express';
-import { createTestClient } from 'apollo-server-testing';
+// import { setupWorker, graphql } from 'msw';
+// import { server } from './index';
+// import { gql } from 'apollo-server-express';
+// import { createTestClient } from 'apollo-server-testing';
 
-const worker = setupWorker();
+// const worker = setupWorker();
--- server/src/test.ts
+++ server/src/test.ts
@@ -1,174 +1,174 @@
-import { ApolloServer, gql } from "apollo-server-express";
-import { createTestClient } from "apollo-server-testing";
-import twitAPI from "./graphql/datasources/twit";
-import userAPI from "./graphql/datasources/user";
-import TwitModel from "./mongoose/twit.model";
-import UserModel from "./mongoose/user.model";
-import userTypeDefs from "./graphql/typeDefs/user";
-import twitTypeDefs from "./graphql/typeDefs/twit";
-import userResolvers from "./graphql/resolvers/user";
-import twitResolvers from "./graphql/resolvers/twit";
-import schema from "./graphql/schema";
-import dataSources from "./graphql/datasources";
+// import { ApolloServer, gql } from "apollo-server-express";
+// import { createTestClient } from "apollo-server-testing";
+// import twitAPI from "./graphql/datasources/twit";
+// import userAPI from "./graphql/datasources/user";
+// import TwitModel from "./mongoose/twit.model";
+// import UserModel from "./mongoose/user.model";
+// import userTypeDefs from "./graphql/typeDefs/user";
+// import twitTypeDefs from "./graphql/typeDefs/twit";
+// import userResolvers from "./graphql/resolvers/user";
+// import twitResolvers from "./graphql/resolvers/twit";
+// import schema from "./graphql/schema";
+// import dataSources from "./graphql/datasources";
 
-const twitts = [{ operation: "add", number: 123 }];
+// const twitts = [{ operation: "add", number: 123 }];
 
-const createApolloServer = () => {
-  return new ApolloServer({
-    /* dataSources: () => ({
-	    twitAPI(): jest.fn()
-    }), */
-    schema,
-    typeDefs: [userTypeDefs, twitTypeDefs],
-    resolvers: [userResolvers, twitResolvers],
-    mocks: true,
-  });
-};
+// const createApolloServer = () => {
+//   return new ApolloServer({
+//     /* dataSources: () => ({
+// 	    twitAPI(): jest.fn()
+//     }), */
+//     schema,
+//     typeDefs: [userTypeDefs, twitTypeDefs],
+//     resolvers: [userResolvers, twitResolvers],
+//     mocks: true,
+//   });
+// };
 
-describe("twits", () => {
-  it("should return twitts", async () => {
-    const server = createApolloServer();
-    const { query } = createTestClient(server);
-
-    const GET_TWITTS = `
-			{
-				twitts {
-					author
-					content {
-						operation
-						number
-					}
-				}
-			}
-		`;
-
-    const response = await query({ query: GET_TWITTS });
-    // console.log('JSON.stringify(response)');
-    // console.log(JSON.stringify(response));
-    expect(response.data?.twitts).toEqual([
-      {
-        content: {
-          operation: "add",
-          number: 123,
-        },
-      },
-    ]);
-  });
-});
-
-// describe('Queries', () => {
-//   it('fetches list of twits', async () => {
-//     // create an instance of ApolloServer that mocks out context, while reusing
-//     // existing dataSources, resolvers, and typeDefs.
-//     // This function returns the server instance as well as our dataSource
-//     // instances, so we can overwrite the underlying fetchers
-//     const { server, userAPI, twitAPI } = constructTestServer({
-//       context: () => ({ user: { id: 1, email: 'a@a.a' } }),
-//     });
-
-//     // mock the datasources' underlying fetch methods, whether that's a REST
-//     // lookup in the RESTDataSource or the store query in the Sequelize datasource
-//     twitAPI.get = jest.fn(() => [mockLaunchResponse]);
-//     userAPI.store = mockStore;
-//     userAPI.store.trips.findAll.mockReturnValueOnce([
-//       { dataValues: { launchId: 1 } },
-//     ]);
-
-//     // use our test server as input to the createTestClient fn
-//     // This will give us an interface, similar to apolloClient.query
-//     // to run queries against our instance of ApolloServer
+// describe("twits", () => {
+//   it("should return twitts", async () => {
+//     const server = createApolloServer();
 //     const { query } = createTestClient(server);
-//     const res = await query({ query: GET_LAUNCHES });
-//     expect(res).toMatchSnapshot();
-//   });
 
-//   it('fetches single launch', async () => {
-//     const { server, launchAPI, userAPI } = constructTestServer({
-//       context: () => ({ user: { id: 1, email: 'a@a.a' } }),
-//     });
+//     const GET_TWITTS = `
+// 			{
+// 				twitts {
+// 					author
+// 					content {
+// 						operation
+// 						number
+// 					}
+// 				}
+// 			}
+// 		`;
 
-//     launchAPI.get = jest.fn(() => [mockLaunchResponse]);
-//     userAPI.store = mockStore;
-//     userAPI.store.trips.findAll.mockReturnValueOnce([
-//       { dataValues: { launchId: 1 } },
+//     const response = await query({ query: GET_TWITTS });
+//     // console.log('JSON.stringify(response)');
+//     // console.log(JSON.stringify(response));
+//     expect(response.data?.twitts).toEqual([
+//       {
+//         content: {
+//           operation: "add",
+//           number: 123,
+//         },
+//       },
 //     ]);
-
-//     const { query } = createTestClient(server);
-//     const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
-//     expect(res).toMatchSnapshot();
 //   });
 // });
 
-// describe('Mutations', () => {
-//   it('returns login token', async () => {
-//     const { server, launchAPI, userAPI } = constructTestServer({
-//       context: () => {},
-//     });
+// // describe('Queries', () => {
+// //   it('fetches list of twits', async () => {
+// //     // create an instance of ApolloServer that mocks out context, while reusing
+// //     // existing dataSources, resolvers, and typeDefs.
+// //     // This function returns the server instance as well as our dataSource
+// //     // instances, so we can overwrite the underlying fetchers
+// //     const { server, userAPI, twitAPI } = constructTestServer({
+// //       context: () => ({ user: { id: 1, email: 'a@a.a' } }),
+// //     });
 
-//     userAPI.store = mockStore;
-//     userAPI.store.users.findOrCreate.mockReturnValueOnce([
-//       { id: 1, email: 'a@a.a' },
-//     ]);
+// //     // mock the datasources' underlying fetch methods, whether that's a REST
+// //     // lookup in the RESTDataSource or the store query in the Sequelize datasource
+// //     twitAPI.get = jest.fn(() => [mockLaunchResponse]);
+// //     userAPI.store = mockStore;
+// //     userAPI.store.trips.findAll.mockReturnValueOnce([
+// //       { dataValues: { launchId: 1 } },
+// //     ]);
 
-//     const { mutate } = createTestClient(server);
-//     const res = await mutate({
-//       mutation: LOGIN,
-//       variables: { email: 'a@a.a' },
-//     });
-//     expect(res.data.login.token).toEqual('YUBhLmE=');
-//   });
+// //     // use our test server as input to the createTestClient fn
+// //     // This will give us an interface, similar to apolloClient.query
+// //     // to run queries against our instance of ApolloServer
+// //     const { query } = createTestClient(server);
+// //     const res = await query({ query: GET_LAUNCHES });
+// //     expect(res).toMatchSnapshot();
+// //   });
 
-//   it('books trips', async () => {
-//     const { server, launchAPI, userAPI } = constructTestServer({
-//       context: () => ({ user: { id: 1, email: 'a@a.a' } }),
-//     });
+// //   it('fetches single launch', async () => {
+// //     const { server, launchAPI, userAPI } = constructTestServer({
+// //       context: () => ({ user: { id: 1, email: 'a@a.a' } }),
+// //     });
 
-//     // mock the underlying fetches
-//     launchAPI.get = jest.fn();
+// //     launchAPI.get = jest.fn(() => [mockLaunchResponse]);
+// //     userAPI.store = mockStore;
+// //     userAPI.store.trips.findAll.mockReturnValueOnce([
+// //       { dataValues: { launchId: 1 } },
+// //     ]);
 
-//     // look up the launches from the launch API
-//     launchAPI.get
-//       .mockReturnValueOnce([mockLaunchResponse])
-//       .mockReturnValueOnce([{ ...mockLaunchResponse, flight_number: 2 }]);
+// //     const { query } = createTestClient(server);
+// //     const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
+// //     expect(res).toMatchSnapshot();
+// //   });
+// // });
 
-//     // book the trip in the store
-//     userAPI.store = mockStore;
-//     userAPI.store.trips.findOrCreate
-//       .mockReturnValueOnce([{ get: () => ({ launchId: 1 }) }])
-//       .mockReturnValueOnce([{ get: () => ({ launchId: 2 }) }]);
+// // describe('Mutations', () => {
+// //   it('returns login token', async () => {
+// //     const { server, launchAPI, userAPI } = constructTestServer({
+// //       context: () => {},
+// //     });
 
-//     // check if user is booked
-//     userAPI.store.trips.findAll.mockReturnValue([{}]);
+// //     userAPI.store = mockStore;
+// //     userAPI.store.users.findOrCreate.mockReturnValueOnce([
+// //       { id: 1, email: 'a@a.a' },
+// //     ]);
 
-//     const { mutate } = createTestClient(server);
-//     const res = await mutate({
-//       mutation: BOOK_TRIPS,
-//       variables: { launchIds: ['1', '2'] },
-//     });
-//     expect(res).toMatchSnapshot();
-//   });
-// });
+// //     const { mutate } = createTestClient(server);
+// //     const res = await mutate({
+// //       mutation: LOGIN,
+// //       variables: { email: 'a@a.a' },
+// //     });
+// //     expect(res.data.login.token).toEqual('YUBhLmE=');
+// //   });
 
-// it('fetches single launch', async () => {
-//   const userAPI = new UserAPI(UserModel.collection);
-//   const twitAPI = new TwitAPI(TwitModel.collection);
+// //   it('books trips', async () => {
+// //     const { server, launchAPI, userAPI } = constructTestServer({
+// //       context: () => ({ user: { id: 1, email: 'a@a.a' } }),
+// //     });
 
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     dataSources: () => ({ userAPI, twitAPI }),
-//     context: () => ({ user: { id: 1, email: 'a@a.a' } }),
-//   });
+// //     // mock the underlying fetches
+// //     launchAPI.get = jest.fn();
 
-//   // mock the dataSource's underlying fetch methods
-//   twitAPI.get = jest.fn(() => [mockLaunchResponse]);
-//   userAPI = mockStore;
-//   userAPI.trips.findAll.mockReturnValueOnce([{ dataValues: { launchId: 1 } }]);
+// //     // look up the launches from the launch API
+// //     launchAPI.get
+// //       .mockReturnValueOnce([mockLaunchResponse])
+// //       .mockReturnValueOnce([{ ...mockLaunchResponse, flight_number: 2 }]);
 
-//   // use the test server to create a query function
-//   const { query } = createTestClient(server);
+// //     // book the trip in the store
+// //     userAPI.store = mockStore;
+// //     userAPI.store.trips.findOrCreate
+// //       .mockReturnValueOnce([{ get: () => ({ launchId: 1 }) }])
+// //       .mockReturnValueOnce([{ get: () => ({ launchId: 2 }) }]);
 
-//   // run query against the server and snapshot the output
-//   const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
-//   expect(res).toMatchSnapshot();
-// });
+// //     // check if user is booked
+// //     userAPI.store.trips.findAll.mockReturnValue([{}]);
+
+// //     const { mutate } = createTestClient(server);
+// //     const res = await mutate({
+// //       mutation: BOOK_TRIPS,
+// //       variables: { launchIds: ['1', '2'] },
+// //     });
+// //     expect(res).toMatchSnapshot();
+// //   });
+// // });
+
+// // it('fetches single launch', async () => {
+// //   const userAPI = new UserAPI(UserModel.collection);
+// //   const twitAPI = new TwitAPI(TwitModel.collection);
+
+// //   const server = new ApolloServer({
+// //     typeDefs,
+// //     resolvers,
+// //     dataSources: () => ({ userAPI, twitAPI }),
+// //     context: () => ({ user: { id: 1, email: 'a@a.a' } }),
+// //   });
+
+// //   // mock the dataSource's underlying fetch methods
+// //   twitAPI.get = jest.fn(() => [mockLaunchResponse]);
+// //   userAPI = mockStore;
+// //   userAPI.trips.findAll.mockReturnValueOnce([{ dataValues: { launchId: 1 } }]);
+
+// //   // use the test server to create a query function
+// //   const { query } = createTestClient(server);
+
+// //   // run query against the server and snapshot the output
+// //   const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
+// //   expect(res).toMatchSnapshot();
+// // });
